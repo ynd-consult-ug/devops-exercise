d9065a9c89801e49e1f2afd442c5c2cf56abeea5
YND <ynd@YNDs-MacBook-Pro.local>
2021-01-07 14:08:02 +0100

    fixed caching problem

--- blog/config/environments/production.rb
+++ blog/config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+
   # Settings specified here will take precedence over those in config/application.rb.
   config.require_master_key = false
   config.read_encrypted_secrets = false
