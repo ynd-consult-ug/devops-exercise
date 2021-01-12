1f72c9935626f4e9a94c289c03e17651d5b12a03
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-12 13:11:26 +0100

    MMmm

--- blog/version.py
+++ blog/version.py
@@ -5,7 +5,8 @@ def version(a):
     with open('mysecret.yaml') as f:
         doc = yaml.load(f)
 
-    doc['stringData']['IMAGE_VERSION'] = "localhost:5000/unoterr1/blog_pipe:" +str(a)
+#   doc['spec']['spec']['stringData']['IMAGE_VERSION'] = "localhost:5000/unoterr1/blog_pipe:" +str(a)
+    doc['spec']['spec']['containers']['image'] = "localhost:5000/unoterr1/blog_pipe:" +str(a)
 
     with open('mysecret.yaml', 'w') as f:
         yaml.dump(doc, f)
