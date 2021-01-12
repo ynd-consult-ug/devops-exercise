c0a65586fab5751159e6c3fd973c5f499ff02e04
unoterr-dev <volodymyr.sorin@ynd.co>
2021-01-12 13:12:40 +0100

    dsad

--- blog/version.py
+++ blog/version.py
@@ -2,13 +2,13 @@ import sys
 import yaml
 
 def version(a):
-    with open('mysecret.yaml') as f:
+    with open('patch.yaml') as f:
         doc = yaml.load(f)
 
 #   doc['spec']['spec']['stringData']['IMAGE_VERSION'] = "localhost:5000/unoterr1/blog_pipe:" +str(a)
     doc['spec']['spec']['containers']['image'] = "localhost:5000/unoterr1/blog_pipe:" +str(a)
 
-    with open('mysecret.yaml', 'w') as f:
+    with open('patch.yaml', 'w') as f:
         yaml.dump(doc, f)
 
 if __name__ == "__main__":
