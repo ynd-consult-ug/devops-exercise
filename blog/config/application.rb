294961bd51f72ca1a646a50b39785d95a89d7112
JakubNajman <najman00@outlook.com>
2021-01-14 16:12:22 +0100

    .

--- blog/config/application.rb
+++ blog/config/application.rb
@@ -18,7 +18,7 @@ module Blog
     config.load_defaults 6.0
     config.lograge.enabled = true
 
-    config.host << '.lvh.me'
+    config.hosts << '.lvh.me'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
